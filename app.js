61e32289c3ec6e8417c0dcb461bcf73c76077ffc
Amit Dubey <amitdubey8888@gmail.com>
2021-01-04 11:16:35 +0530

    new changes in app

--- app.js
+++ app.js
@@ -52,24 +52,3 @@ app.get('/', function(req, res) {
 
 app.listen(port);
 console.log('Backend server is running on http://localhost:3000');
-
-// Web Socket Connection Start
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({
-  port: 9000
-});
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    const chatData = JSON.parse(data);
-    chatController.liveChat(chatData);
-    wss.clients.forEach(function each(client) {
-      if (chatData['receiver'] == client.protocol) {
-        if (client.readyState == WebSocket.OPEN) {
-          client.send(JSON.stringify(chatData));
-        }
-      }
-    });
-  });
-});
-// Web Socket Connection End
-console.log('Socket server is running on ws://localhost:9000');
